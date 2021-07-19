859d0bbff6e5462a22fd8af0a3ec0d1ccc84a662
kimbokChi <han15050424@gmail.com>
2021-07-19 15:53:10 +0900

    Revert "Galaxy클래스의 도움 없이 운석 아이템을 구현한다"
    This reverts commit 8429368f23907b4371f390c7e1d5439cd016df63.

--- Assets/Scripts/Items/Intance/Meteorite.cs
+++ Assets/Scripts/Items/Intance/Meteorite.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public partial class Meteorite : Item
+public class Meteorite : Item
 {
     [Header("Meteorh Property")]
     [SerializeField] private float _Speed;
     [SerializeField] private float _Radius;
     [Space()]
     [SerializeField] private Area _Meteore;
-    [SerializeField] private Transform _OrbitObject;
 
     private GameObject _Player;
-    private IEnumerator _Resolving;
-
-    private bool _IsAlreadyInit = false;
 
     public override void AttackCancel()
     { }
@@ -24,41 +20,23 @@ public partial class Meteorite : Item
         {
             case SlotType.Accessory:
                 {
-                    DisableMeteorite();
+                    Galaxy.Instance.RemoveMeteorite();
                 }
                 break;
             case SlotType.Weapon:
                 {
-                    DisableMeteorite(3);
+                    Galaxy.Instance.RemoveMeteorite(3);
                 }
                 break;
         }
     }
     public override void OnEquipThis(SlotType onSlot)
     {
-        InitMethod();
-
-        switch (onSlot)
-        {
-            case SlotType.Accessory:
-                {
-                    EnableMeteorite();
-                }
-                break;
-            case SlotType.Weapon:
-                {
-                    EnableMeteorite(3);
-                }
-                break;
-        }
-    }
-    private void InitMethod()
-    {
-        if (!_IsAlreadyInit)
+        if (!Galaxy.Instance.IsAlreadyInit)
         {
             _Player = GameObject.FindGameObjectWithTag("Player");
 
-            _MeteoPool.Init(3, _Meteore, meteo =>
+            Galaxy.Instance.LazyInit(_Meteore, meteo =>
             {
                 meteo.SetEnterAction(enter => {
 
@@ -72,78 +50,22 @@ public partial class Meteorite : Item
                     }
                 });
             });
-            _IsAlreadyInit = true;
-            _OrbitObject.SetParent(null);
+            Galaxy.Instance.Resolving_Radius = _Radius;
+            Galaxy.Instance.Resolving_Speed  = _Speed;
         }
-    }
-    private IEnumerator ResolvingRoutine()
-    {
-        float angle = 0f;
 
-        _OrbitObject.gameObject.SetActive(true);
-        do
+        switch (onSlot)
         {
-            float angleDistance = 360f / _EnabledMeteoList.Count;
-
-            angle += Time.deltaTime * Time.timeScale * _Speed * Inventory.Instance.AttackSpeed;
-            angle = Mathf.Abs(angle) > 360f ? angle % 360f : angle;
-
-            for (int i = 0; i < _EnabledMeteoList.Count; ++i)
-            {
-                float rot = (angle + angleDistance * i + 90f * Mathf.Sign(_Speed)) * Mathf.Deg2Rad;
-
-                _EnabledMeteoList[i].transform.localPosition =
-                    new Vector2(Mathf.Cos(rot), Mathf.Sin(rot)) * _Radius;
-
-                _EnabledMeteoList[i].transform.localRotation =
-                    Quaternion.AngleAxis(angle + angleDistance * i, Vector3.forward);
-            }
-            yield return null;
-
-            _OrbitObject.localPosition = _Player.transform.localPosition;
-
-        } while (_EnabledMeteoList.Count != 0);
-
-        _Resolving = null;
-        _OrbitObject.gameObject.SetActive(false);
-    }
-}
-
-public partial class Meteorite
-{
-    private Pool<Area> _MeteoPool 
-        = new Pool<Area>();
-
-    private List<Area> _EnabledMeteoList
-         = new List<Area>();
-
-    private void EnableMeteorite(int count = 1)
-    {
-        var meteor = _MeteoPool.Get();
-
-        _EnabledMeteoList.Add(meteor);
-
-        meteor.transform.parent = _OrbitObject;
-        meteor.transform.localScale = Vector3.one;
-
-        if (--count > 0)
-        {
-            EnableMeteorite(count);
+            case SlotType.Accessory:
+                {
+                    Galaxy.Instance.AddMeteorite();
+                }
+                break;
+            case SlotType.Weapon:
+                {
+                    Galaxy.Instance.AddMeteorite(3);
+                }
+                break;
         }
-        if (_Resolving == null)
-        {
-            StartCoroutine(_Resolving = ResolvingRoutine());
-        }
-    }
-    private void DisableMeteorite(int count = 1)
-    {
-        _MeteoPool.Add(_EnabledMeteoList[0]);
-        _EnabledMeteoList.RemoveAt(0);
-
-        if (--count > 0)
-        {
-            DisableMeteorite(count);
-        }
-        return;
     }
 }
