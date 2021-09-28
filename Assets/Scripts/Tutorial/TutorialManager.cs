931d193202d296906e7df1c4fc62cf470d33483e
kimbokChi <han15050424@gmail.com>
2021-09-28 20:22:09 +0900

    튜토리얼 매니저에 다음 다이얼로그를 출력하는 기능을 추가한다
    이전에 다이얼로그에게 추가한 이벤트를 구독하여, 다음 다이얼로그를 출력하는 기능을
    구현했다!

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -5,5 +5,39 @@ using UnityEngine;
 
 public class TutorialManager : Singleton<TutorialManager>
 {
-    
+    [SerializeField] private DialogData _TutorialDialog;
+
+    private int _CurrentGroupIndex = 1;
+    private Queue<DialogIndex> _DialogQueue = new Queue<DialogIndex>();
+
+    private void Awake()
+    {
+        Dialog.Instance.OnTouchOutputFinish += () => 
+        {
+            if (_DialogQueue.Count == 0) {
+                MakeDialogGroup(++_CurrentGroupIndex);
+
+                // 출력할 수 있는 로그를 모두 출력했다면
+                if (_DialogQueue.Count == 0)
+                {
+                    Dialog.Instance.CloseLog();
+                    return;
+                }
+            }
+            var logData = _DialogQueue.Dequeue();
+            Dialog.Instance.WriteLog(logData.Name, logData.Text, null);
+        };
+    }
+
+    // 입력한 인덱스의 다이얼로그 그룹을 형성한다.
+    private void MakeDialogGroup(int groupIndex)
+    {
+        foreach (var data in _TutorialDialog.Data)
+        {
+            // 정렬되어있다는 가정하에...
+            if (data.GroupCode == groupIndex) {
+                _DialogQueue.Enqueue(data);
+            }
+        }
+    }
 }
