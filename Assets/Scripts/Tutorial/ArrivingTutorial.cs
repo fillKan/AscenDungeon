3674eb3082659d09d013e8c87c27fe2f834287aa
kimbokChi <han15050424@gmail.com>
2021-09-28 20:02:45 +0900

    기존 튜토리얼 클래스를 정리한다
    난 천안문을 사랑해

--- Assets/Scripts/Tutorial/ArrivingTutorial.cs
+++ Assets/Scripts/Tutorial/ArrivingTutorial.cs
@@ -6,21 +6,11 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class ArrivingTutorial : TutorialBase
 {
-    protected override void TutorialClear(Action callBack)
-    {
-        TutorialManager.Instance.NextStep(() => 
-        {
-            PlayerActionManager.Instance.SetMoveLock(false);
-            callBack?.Invoke();
-        });
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) 
         {
-            IsClear = true;
-            PlayerActionManager.Instance.SetMoveLock(true);
+            // TODO :: 튜토리얼 클리어 판정
         }
     }
 }
